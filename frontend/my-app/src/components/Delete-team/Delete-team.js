8e55660aaee4747f27040b31d5980623058ab82d
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-25 17:09:32 -0300

    Updates DleteTeam component
    updates the effect to send the correct params to custom use effect

--- frontend/my-app/src/components/Delete-team/Delete-team.js
+++ frontend/my-app/src/components/Delete-team/Delete-team.js
@@ -1,12 +1,14 @@
 import React from "react";
 import deleteTeamUseffect from "../../hooks/deleteTeamHook";
 import Loading from "../Loading";
-
+import { useParams } from "react-router-dom";
+import { deleteTeam } from "../../api/teams";
+import mapTeamsList from "../../mapper/mapper";
 
 
 const DeleteTeam = () => {
-
-	deleteTeamUseffect();
+	const { teamId } = useParams();
+	deleteTeamUseffect(teamId, deleteTeam, mapTeamsList);
 
 	return (
 		<Loading loading={true}/>
