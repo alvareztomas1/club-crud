2401d5d8564e50b0933bf8d1fa710d10adebba69
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-25 17:10:27 -0300

    Updates RestoreDatabase component
    Adds the correct params to the component custom hook

--- frontend/my-app/src/components/Restore-database/Restore-database.js
+++ frontend/my-app/src/components/Restore-database/Restore-database.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Loading from "../Loading";
 import restoreDataBaseHook from "../../hooks/restoreDatabaseHook";
-
+import { getRestoredDatabase } from "../../api/teams";
+import mapTeamsList from "../../mapper/mapper";
 
 
 const RestoreDatabase = () => {
 
-	restoreDataBaseHook();
+	restoreDataBaseHook(getRestoredDatabase, mapTeamsList);
 
 	return (
 		<Loading loading={true}/>
