90654fcc6028379ab890e7489362560e068ac666
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-25 17:18:46 -0300

    Adds cypress testing to 'delete a team' operation

---
+++ frontend/my-app/cypress/e2e/delete-team.cy.js
@@ -0,0 +1,43 @@
+///<reference types="cypress" />
+
+describe("delete a team", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000/");
+
+		cy.intercept("GET", "http://localhost:8080/teams", {
+			fixture: "teams.json"
+		}).as("getTeams");
+
+		cy.intercept("DELETE", "http://localhost:8080/delete/57", {
+			fixture: "delete-team.json"
+		}).as("deleteTeam");
+	});
+
+	it("verifies deleting a team", () => {
+		cy.get("#ARS-57-logo").should("exist");
+		cy.get("#ARS-57-name").should("exist");
+		cy.get("#57-info-button").should("exist");
+		cy.get("#show-57-modal-button").should("exist");
+		cy.get("#57-edit-button").should("exist");
+
+		cy.intercept("GET", "http://localhost:8080/teams", {
+			fixture: "delete-team.json"
+		}).as("getNewTeamsList");
+
+		cy.get("#show-57-modal-button").click();
+		cy.get("#57-delete-button").click();
+
+		
+		cy.get("#show-57-modal-button").should("not.exist");
+		cy.get("#ARS-57-logo").should("not.exist");
+		cy.get("#ARS-57-name").should("not.exist");
+		cy.get("#57-info-button").should("not.exist");
+		cy.get("#57-edit-button").should("not.exist");
+
+		cy.get("#toast").should("be.visible");
+		cy.get("#toast").should("have.class", "toast align-items-center text-bg-danger border-0");
+		cy.get("#toast").should("not.exist");
+
+
+	});
+});
