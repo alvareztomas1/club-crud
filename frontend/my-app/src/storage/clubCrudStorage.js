fe3ebeea8a4265f5e90d585f30db3db8d186c931
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-04-01 11:24:21 -0300

    Removes unnecessary comment

--- frontend/my-app/src/storage/clubCrudStorage.js
+++ frontend/my-app/src/storage/clubCrudStorage.js
@@ -1,27 +1,3 @@
-/*export function savePokemons(offset, limit, pokemons){
-    if(offset === undefined || limit === undefined || typeof(pokemons) !== "object"){
-        throw new Error("offset and limit must  be defined");
-    }
-    try{
-        localStorage.setItem(`pokemons_${offset}_${limit}`, JSON.stringify(pokemons));
-
-    }catch(e){
-        localStorage.clear();
-    }
-};
-
-export function loadPokemons(offset, limit){
-    if(offset === undefined || limit === undefined){
-        throw new Error("offset and limit must  be defined");
-    };
-
-    const pokemons = JSON.parse(localStorage.getItem(`pokemons_${offset}_${limit}`));
-
-    return pokemons;
-};
-
-*/
-
 export function loadTeams(){
 	const teams = JSON.parse(localStorage.getItem("teams"));
 	if(!teams){
